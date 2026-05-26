626d016dc1e4cf0ec195cb7b43550436e4b76920
benjypng <47936793+benjypng@users.noreply.github.com>
2026-05-26 22:22:09 +0800

    Revert "fix: pin app height to window.innerHeight on ios"
    This reverts commit 3b8646789919b7dde14fbb478b1ac5008f52ea77.

--- src/main.tsx
+++ src/main.tsx
@@ -7,16 +7,6 @@ import './index.css'
 
 const queryClient = new QueryClient()
 
-const setAppHeight = () => {
-  document.documentElement.style.setProperty(
-    '--app-height',
-    `${window.innerHeight}px`,
-  )
-}
-setAppHeight()
-window.addEventListener('resize', setAppHeight)
-window.addEventListener('orientationchange', setAppHeight)
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
