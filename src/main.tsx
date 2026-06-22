f71d1d6d3a4b595c390fbc6d5b854706621e61f6
benjypng <47936793+benjypng@users.noreply.github.com>
2026-06-22 23:03:05 +0800

    fix: remove auth

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-import { AuthGate } from './components/AuthGate'
 import { isIosStandalone, nudgeViewportStaggered } from './utils/ios-pwa'
 
 const queryClient = new QueryClient()
@@ -20,9 +19,7 @@ if (isIosStandalone()) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthGate>
-        <App />
-      </AuthGate>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 )
